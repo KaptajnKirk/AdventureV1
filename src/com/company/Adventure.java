38c88077a767ca5bb1f087d3ff85da0fe9d577ce
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-14 13:48:12 +0100

    First commit

---
+++ src/com/company/Adventure.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Adventure {
+
+    Scanner scanner = new Scanner(System.in);
+    String North;
+    String East;
+    String South;
+    String West;
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/com/company/Rooms.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Rooms {
+
+  //Create 9 rooms as objects with entrances that connect to different rooms
+
+  //Type connection to the room with array = room1.addDirection("south", rooms[0]);
+
+  //
+
+}
