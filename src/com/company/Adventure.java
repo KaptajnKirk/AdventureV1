4df16ed7345388580a5fa35eca553cabfd2d1869
Linding3r <thomasjuullindinger@yahoo.dk>
2022-03-15 12:45:09 +0100

    seperated displayRoomDescription and displayLookDescription

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 public class Adventure {
 
   Scanner scanner = new Scanner(System.in);
-  Rooms room1 = new Rooms("You are in the Captains quarters", "You awake in a room partly on fire. Small pieces of debris is falling from holes in the ceiling. Around you are scattered furniture, displaced in the crash. A work table, some vials on the floor, and a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine. You are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser. You see doors to the south and east");
-  Rooms room2 = new Rooms("You are in the Mess Hall", "You walk into the massive mess hall, made to feed the 240 workers aboard the Alterra Starliner Aurora. Only a few of the bright fluorecent lights, light up the grey and white walls of the mess hall, and reflect in the water that is slowly filling the room. Once filled with perfectly formed lines of long-tables and benches, the room is now a chaos of floating furniture and patches of fire. You see doors to the west and east");
-  Rooms room3 = new Rooms("You are in the Armory", "You walk into a medium sized room, with water reaching to your knees. The room is tightly packed with shelves that are now partly empty, as the crates that once occupied them are now all around you. Contrary to its name, you dont see any actual weapons, but mostly tools used for excavation and research. The room is slightly dark, as your only light-source is a standing lamp, now tipped over and lying submerged in water, scattering the light. You see doors to the west and south");
-  Rooms room4 = new Rooms("You are in the Garage ", "You walk into the giant garage, with cars, excavation-machines and boats are hanging from the ceiling. Some of the bigger vehicles have fallen to the ground, but somehow the garage is weirdly intact. A small electric fire obscures one of the exits. There is a giant gate at the end of the room, for getting the vehicles out, once on planet-side. You know it leads to the Auroras stern ramp. the gates is slowly leaking water through the crack where to two doors meet. You see doors to the north and south");
-  Rooms room5 = new Rooms("You are in the Reactor Room", "You walk onto the bridge of the reactor room. The bridge is suspended in the middle of the massive room, overlooking the abyss that is now the almost filled reactor room. The water almost reaches the bridge. Four pillars stretches from the ceiling way above you, and disappear into the depths below. When you look closely, you can see the shadow of something big, slithering around in the water below you, but you cant quite make out what it is. The way you came in, seems to be the only entrance or exit");
-  Rooms room6 = new Rooms("You are in the Engine Room", "You walk into, what you instantly recognize as the engine room, since this is where you used to work. its a low hanging ceiling, and you have to duck some places to get around, because of the interwoven tubes that go from the engines to the sub-engines. The room is mostly intact, with the exception of small fires here and there, and one of the sub-engines leaking coolant. You see a door to the north and south");
-  Rooms room7 = new Rooms("You are in the Sleeping Quarters", "You walk into the sleeping quarters. This is just one room of the many that makes up the crew quarters. Small fires are scattered across the room, and a medium sizes hole has been made in the southern wall, making the neighbour room visible. As Alterra doesnt allow many personal items to bring along on excavation voyages, the only things floating around in the ankle high water are small tools, hygiene items, papers, etc. You see doors to the north and east");
-  Rooms room8 = new Rooms("You are in the Science Lab", "You walk into a small room, with a single, one-person submarine suspended from the ceiling in the middle of the room. You have never visited the lab prior, since it has always been off limits. Glass shards from vials fills the floor, and microscopes and other research tools, are scattered amongst the flipped worktables and whiteboards. You can see equations etched onto the wall-embedded worktables. The ceiling lights are weirdly still fully functioning, covering the lab in a bright defused, white light. You see doors to the west, north and east");
-  Rooms room9 = new Rooms("You are in the Submarine Bay", "You walk into a massive hall, with Alterra's signature expedition submarine, 'The Cyclops' taking up most of the hall. The submarine has fallen out of its ceiling mount, and now partly blocks the entrance pool below it. On the bottom of the entrance pool, there is a gate that opens up, as an exit for submarines. Around the room, there are one-man submarines socketed into the walls, where they used to charge. Some of them fell out during the crash, and now partly occupy the bay floor. You should be able to take a one-man submarine through the gate, provided you can get it open. you see doors to the north and west.");
+  Rooms room1 = new Rooms("Captains quarters", " You awake in a room partly on fire. Small pieces of debris is falling from holes in the ceiling. Around you are scattered furniture, displaced in the crash. A work table, some vials on the floor, and a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine. You are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser. You see doors to the south and east");
+  Rooms room2 = new Rooms("Mess Hall", " the massive mess hall, made to feed the 240 workers aboard the Alterra Starliner Aurora. Only a few of the bright fluorecent lights, light up the grey and white walls of the mess hall, and reflect in the water that is slowly filling the room. Once filled with perfectly formed lines of long-tables and benches, the room is now a chaos of floating furniture and patches of fire. You see doors to the west and east");
+  Rooms room3 = new Rooms("Armory", " a medium sized room, with water reaching to your knees. The room is tightly packed with shelves that are now partly empty, as the crates that once occupied them are now all around you. Contrary to its name, you dont see any actual weapons, but mostly tools used for excavation and research. The room is slightly dark, as your only light-source is a standing lamp, now tipped over and lying submerged in water, scattering the light. You see doors to the west and south");
+  Rooms room4 = new Rooms("Garage ", " the giant garage, with cars, excavation-machines and boats are hanging from the ceiling. Some of the bigger vehicles have fallen to the ground, but somehow the garage is weirdly intact. A small electric fire obscures one of the exits. There is a giant gate at the end of the room, for getting the vehicles out, once on planet-side. You know it leads to the Auroras stern ramp. the gates is slowly leaking water through the crack where to two doors meet. You see doors to the north and south");
+  Rooms room5 = new Rooms("Reactor Room", " the massive reactor room, onto the bridge. The bridge is suspended in the middle of the massive room, overlooking the abyss that is now the almost filled reactor room. The water almost reaches the bridge. Four pillars stretches from the ceiling way above you, and disappear into the depths below. When you look closely, you can see the shadow of something big, slithering around in the water below you, but you cant quite make out what it is. The way you came in, seems to be the only entrance or exit");
+  Rooms room6 = new Rooms("Engine Room", ", what you instantly recognize as the engine room, since this is where you used to work. its a low hanging ceiling, and you have to duck some places to get around, because of the interwoven tubes that go from the engines to the sub-engines. The room is mostly intact, with the exception of small fires here and there, and one of the sub-engines leaking coolant. You see a door to the north and south");
+  Rooms room7 = new Rooms("Sleeping Quarters", " the sleeping quarters. This is just one room of the many that makes up the crew quarters. Small fires are scattered across the room, and a medium sizes hole has been made in the southern wall, making the neighbour room visible. As Alterra doesnt allow many personal items to bring along on excavation voyages, the only things floating around in the ankle high water are small tools, hygiene items, papers, etc. You see doors to the north and east");
+  Rooms room8 = new Rooms("Science Lab", " a small room, with a single, one-person submarine suspended from the ceiling in the middle of the room. You have never visited the lab prior, since it has always been off limits. Glass shards from vials fills the floor, and microscopes and other research tools, are scattered amongst the flipped worktables and whiteboards. You can see equations etched onto the wall-embedded worktables. The ceiling lights are weirdly still fully functioning, covering the lab in a bright defused, white light. You see doors to the west, north and east");
+  Rooms room9 = new Rooms("Submarine Bay", " a massive hall, with Alterra's signature expedition submarine, 'The Cyclops' taking up most of the hall. The submarine has fallen out of its ceiling mount, and now partly blocks the entrance pool below it. On the bottom of the entrance pool, there is a gate that opens up, as an exit for submarines. Around the room, there are one-man submarines socketed into the walls, where they used to charge. Some of them fell out during the crash, and now partly occupy the bay floor. You should be able to take a one-man submarine through the gate, provided you can get it open. you see doors to the north and west.");
 
   Rooms currentLocation = room1;
+  boolean discovered = currentLocation.getDiscovered();
 
   public void roomRoutes() {
     room1.setDiscovered(true); //As it is the room that you spawn in
@@ -39,28 +40,30 @@ public class Adventure {
     room9.setNorth(room6);
   }
 
-  public void roomName() {
-    System.out.println(currentLocation.getName());
+  public void displayRoomName() {
+    System.out.println("You are in the " + currentLocation.getName());
   }
 
-  public void roomDescription() {
-    System.out.println(currentLocation.getDescription());
+  public void displayRoomDescription() {
+    System.out.print("You walk into" + currentLocation.getDescription());
   }
 
-
-  public void intro() {
-    System.out.println();
-    roomDescription();
+  public void displayLookDescription(){
+    System.out.println("You are in" + currentLocation.getDescription());
   }
 
-  public void helpMenu() {
+  public void displayIntro() {
+    displayRoomDescription();
+  }
+
+  public void displayHelpMenu() {
     System.out.println("exit\t- Exit the game");
     System.out.println("look\t- Get the description of current room");
     System.out.println("go\t\t- Go in the direction you wish by typing 'go' followed by the direction. (ex. go north)");
     System.out.println("help\t- Get this help menu");
   }
 
-  public void areaDiscovered() {
+  public void markAreaDiscovered() {
     currentLocation.setDiscovered(true);
   }
 
@@ -69,10 +72,11 @@ public class Adventure {
       System.out.println("You cannot go this way");
     } else {
       currentLocation = currentLocation.getNorth();
-    roomName();}
+      displayRoomName();
+    }
     if (currentLocation.getDiscovered() == false) {
-      areaDiscovered();
-      roomDescription();
+      markAreaDiscovered();
+      displayRoomDescription();
     }
   }
 
@@ -81,10 +85,10 @@ public class Adventure {
       System.out.println("You cannot go this way");
     } else{
       currentLocation = currentLocation.getSouth();
-    roomName();}
+    displayRoomName();}
     if (currentLocation.getDiscovered() == false) {
-      areaDiscovered();
-      roomDescription();
+      markAreaDiscovered();
+      displayRoomDescription();
     }
   }
 
@@ -93,10 +97,10 @@ public class Adventure {
       System.out.println("You cannot go this way");
     } else {
       currentLocation = currentLocation.getEast();
-    roomName();}
+    displayRoomName();}
     if (currentLocation.getDiscovered() == false) {
-      areaDiscovered();
-      roomDescription();
+      markAreaDiscovered();
+      displayRoomDescription();
     }
   }
 
@@ -105,22 +109,22 @@ public class Adventure {
       System.out.println("You cannot go this way");
     } else{
       currentLocation = currentLocation.getWest();
-    roomName();}
+    displayRoomName();}
     if (currentLocation.getDiscovered() == false) {
-      areaDiscovered();
-      roomDescription();
+      markAreaDiscovered();
+      displayRoomDescription();
     }
   }
 
   public void game() {
-    System.out.println("Welcome to Subnautica");
-    System.out.println("\n Press ENTER to continue");
     roomRoutes();
+    System.out.println("Welcome to Subnautica");
+    System.out.println("\nPress ENTER to continue");
     String next = scanner.nextLine();
-    intro();
+    displayIntro();
     boolean run = true;
     while (run) {
-      System.out.println("What is your next move?");
+      System.out.println("\n\nWhat is your next move?");
       String choice = scanner.nextLine();
 
       switch (choice) {
@@ -129,7 +133,7 @@ public class Adventure {
           run = false;
           break;
         case "look":
-          roomDescription();
+          displayLookDescription();
           break;
         case "go north", "north", "n":
           goNorth();
@@ -148,7 +152,7 @@ public class Adventure {
           System.out.println("Please type something like 'go east' or 'go north'");
           break;
         case "help":
-          helpMenu();
+          displayHelpMenu();
           break;
         default:
           System.out.println("Invalid input");
