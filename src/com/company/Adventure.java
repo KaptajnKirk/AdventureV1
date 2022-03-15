0ae4a361d954519e3c4181b4a326592a3341d99c
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-15 16:51:29 +0100

    Added Backstory and a "Wake up" sequence, along with autoprinted help menu at the start

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -84,8 +84,14 @@ public class Adventure {
     System.out.println("You are in" + currentLocation.getDescription());
   }
 
+  //Intro to the games backstory and setup
+  public void displayIntro(){
+    System.out.println("\u001B[32m***************************************[BACKSTORY]*****************************************************\u001B[0m");
+    System.out.println("You are an engine mechanic aboard the galactic excavation and discovery vehicle The Aurora.\nThe mission, codenamed 'Degassi' is funded by the trans-governmental corporation Alterra.\nAlterra owns 61% of all space travel assets in the milky way galaxy, and they also happen to be your employer. \nThe Degassi mission is slated to last approximately four years. \nits main objective is to research and excavate newly discovered planet 6784X, for future habitation. \nWith no real family or relationship back on earth, you signed on for the generous mission salary. \nThe Aurora is a massive ship equipped for both land and water-based missions, and houses 265 crewmembers in total. \nThe Degassi mission has recently passed its one year mark, and you are currently slingshotting around planet 4546B. \nAn ocean planet consisting of 91.1% water. \nOne day you are called to the Captains Quarters, to report the weekly fuel consumption rate. \nIn the middle of your report, the ship suddenly starts to shake violently, and you lose your footing, stumbling to the ground. \nRed lights starts blinking, as the intercom sounds 'ALERT: SHIP UNDER ATTACK. TWO MAIN ENGINES LOST. EVACUATE TO THE ESCAPE PODS'. \nSuddenly a small explosion happens inside the room, and as you and the captain are storming towards the exit, a single wall panel rips lose. \nit flies straight towards you. In an instant, everything turns dark...");
+  }
+
   //Intro to the game, basically room 1 description but with a wakeup start sentence
-  public void displayIntro() {
+  public void displayWakeUp() {
     System.out.println("You awake in a room partly on fire. Small pieces of debris is falling from holes in the ceiling.\nAround you are scattered furniture, displaced in the crash. A work table, some vials on the floor,\nand a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine.\nYou are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser.\nYou see doors to the south and east.");
   }
 
@@ -173,7 +179,15 @@ public class Adventure {
     displayTitleCard();
     System.out.println("\nPress ENTER to continue");
     String next = scanner.nextLine();
+    System.out.println("These are the following commands you can use, during the adventure");
+    displayHelpMenu();
     displayIntro();
+    System.out.println("\nType 'wake up' to start the adventure");
+    String wakeUp = scanner.nextLine();
+    switch (wakeUp) {
+      case "wake up":
+        displayWakeUp();
+        break;}
     boolean run = true;
     while (run) {
       System.out.println("\n\nWhat is your next move?");
