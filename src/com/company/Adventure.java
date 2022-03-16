918ad96719b14ef5b16215d2dd04ec4ac9763f8b
Linding3r <thomasjuullindinger@yahoo.dk>
2022-03-16 14:29:07 +0100

    removed all 4 go methodes(goWest, goEast etc) and made one goDirection to shorten the code. Also changed some of the long Strings

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -7,7 +7,7 @@ public class Adventure {
   Scanner scanner = new Scanner(System.in);
 
   //Create all 9 rooms for the game
-  Rooms room1 = new Rooms("Captains quarters", "  a room partly on fire. Small pieces of debris is falling from holes in the ceiling.\nAround you are scattered furniture, displaced in the crash. A work table, some vials on the floor,\nand a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine.\nYou are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser.\nYou see doors to the south and east.");
+  Rooms room1 = new Rooms("Captains quarters", " a room partly on fire. Small pieces of debris is falling from holes in the ceiling.\nAround you are scattered furniture, displaced in the crash. A work table, some vials on the floor,\nand a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine.\nYou are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser.\nYou see doors to the south and east.");
   Rooms room2 = new Rooms("Mess Hall", " the massive mess hall, made to feed the 240 workers aboard the Alterra Starliner Aurora.\nOnly a few of the bright fluorecent lights, light up the grey and white walls of the mess hall,\nand reflect in the water that is slowly filling the room. Once filled with perfectly formed lines of long-tables and benches,\nthe room is now a chaos of floating furniture and patches of fire.\nYou see doors to the west and east.");
   Rooms room3 = new Rooms("Armory", " a medium sized room, with water reaching to your knees. The room is tightly packed with shelves that are now partly empty,\nas the crates that once occupied them are now all around you. Contrary to its name, you dont see any actual weapons,\nbut mostly tools used for excavation and research. The room is slightly dark, as your only light-source is a standing lamp,\nnow tipped over and lying submerged in water, scattering the light.\nYou see doors to the west and south.");
   Rooms room4 = new Rooms("Garage ", " the giant garage, with cars, excavation-machines and boats are hanging from the ceiling.\nSome of the bigger vehicles have fallen to the ground, but somehow the garage is weirdly intact.\nA small electric fire obscures one of the exits. There is a giant gate at the end of the room, for getting the vehicles out,\nonce on planet-side. You know it leads to the Auroras stern ramp.\nThe gates is slowly leaking water through the crack where to two doors meet.\nYou see doors to the north and south.");
@@ -87,28 +87,32 @@ public class Adventure {
   //Intro to the games backstory and setup
   public void displayIntro() {
     System.out.println("\u001B[32m***************************************[BACKSTORY]*****************************************************\u001B[0m");
-    System.out.println("You are an engine mechanic aboard the galactic excavation and discovery vehicle The Aurora." +
-        "\nThe mission, codenamed 'Degassi' is funded by the trans-governmental corporation Alterra." +
-        "\nAlterra owns 61% of all space travel assets in the milky way galaxy, and they also happen to be your employer." +
-        "\nThe Degassi mission is slated to last approximately four years." +
-        "\nits main objective is to research and excavate newly discovered planet 6784X, for future habitation." +
-        "\nWith no real family or relationship back on earth, you signed on for the generous mission salary." +
-        "\nThe Aurora is a massive ship equipped for both land and water-based missions, and houses 265 crewmembers in total. " +
-        "\nThe Degassi mission has recently passed its one year mark, and you are currently slingshotting around planet 4546B. " +
-        "\nAn ocean planet consisting of 91.1% water. \nOne day you are called to the Captains Quarters, to report the weekly fuel consumption rate. " +
-        "\nIn the middle of your report, the ship suddenly starts to shake violently, and you lose your footing, stumbling to the ground. " +
-        "\nRed lights starts blinking, as the intercom sounds 'ALERT: SHIP UNDER ATTACK. TWO MAIN ENGINES LOST. EVACUATE TO THE ESCAPE PODS'. " +
-        "\nSuddenly a small explosion happens inside the room, and as you and the captain are storming towards the exit, a single wall panel rips lose. " +
-        "\nit flies straight towards you. In an instant, everything turns dark...");
+    System.out.println("""
+            You are an engine mechanic aboard the galactic excavation and discovery vehicle The Aurora.
+            The mission, codenamed 'Degassi' is funded by the trans-governmental corporation Alterra.
+            Alterra owns 61% of all space travel assets in the milky way galaxy, and they also happen to be your employer.
+            The Degassi mission is slated to last approximately four years.
+            its main objective is to research and excavate newly discovered planet 6784X, for future habitation.
+            With no real family or relationship back on earth, you signed on for the generous mission salary.
+            The Aurora is a massive ship equipped for both land and water-based missions, and houses 265 crew members in total.
+            The Degassi mission has recently passed its one year mark, and you are currently slingshotting around planet 4546B.
+            An ocean planet consisting of 91.1% water. One day you are called to the Captains Quarters, to report the weekly fuel consumption rate.
+            In the middle of your report, the ship suddenly starts to shake violently, and you lose your footing, stumbling to the ground.
+            Red lights starts blinking, as the intercom sounds 'ALERT: SHIP UNDER ATTACK. TWO MAIN ENGINES LOST. EVACUATE TO THE ESCAPE PODS'.
+            Suddenly a small explosion happens inside the room, and as you and the captain are storming towards the exit, a single wall panel rips lose.
+            it flies straight towards you. In an instant, everything turns dark...
+            """);
   }
 
   //Intro to the game, basically room 1 description but with a wakeup start sentence
   public void displayWakeUp() {
-    System.out.println("You awake in a room partly on fire. Small pieces of debris is falling from holes in the ceiling." +
-        "\nAround you are scattered furniture, displaced in the crash. A work table, some vials on the floor," +
-        "\nand a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine." +
-        "\nYou are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser." +
-        "\nYou see doors to the south and east.");
+    System.out.println("""
+            You awake in a room partly on fire. Small pieces of debris is falling from holes in the ceiling.
+            Around you are scattered furniture, displaced in the crash. A work table, some vials on the floor,
+            and a Alterra poster on the wall with a picture of the newest model of Alterra excavation machine.
+            You are slightly confused, and the last thing you remember is dropping to the planet surface in the Alterra Space cruiser.
+            You see doors to the south and east.
+            """);
   }
 
   //display of the HELP menu
@@ -129,60 +133,11 @@ public class Adventure {
     currentLocation.setDiscovered(true);
   }
 
-  //Get user to go north if there is a room in that direction, if not it tells the user that they can't go that way
-  public void goNorth() {
-    if (currentLocation.getNorth() == null) {
+  public void goDirection(Rooms direction){
+    if (direction == null) {
       System.out.println("You cannot go this way");
     } else {
-      currentLocation = currentLocation.getNorth();
-    }
-    if (currentLocation.getDiscovered()) { //Do not display room name when direction is null
-      displayRoomName();
-    }else {
-      markAreaDiscovered();
-      displayRoomDescription();
-      displayRoomName();
-    }
-  }
-
-  //Get user to go south if there is a room in that direction, if not it tells the user that they can't go that way
-  public void goSouth() {
-    if (currentLocation.getSouth() == null) {
-      System.out.println("You cannot go this way");
-    } else {
-      currentLocation = currentLocation.getSouth();
-    }
-    if (currentLocation.getDiscovered()) { //Do not display room name when direction is null
-      displayRoomName();
-    }else {
-      markAreaDiscovered();
-      displayRoomDescription();
-      displayRoomName();
-    }
-  }
-
-  //Get user to go east if there is a room in that direction, if not it tells the user that they can't go that way
-  public void goEast() {
-    if (currentLocation.getEast() == null) {
-      System.out.println("You cannot go this way");
-    } else {
-      currentLocation = currentLocation.getEast();
-    }
-    if (currentLocation.getDiscovered()) { //Do not display room name when direction is null
-      displayRoomName();
-    }else {
-      markAreaDiscovered();
-      displayRoomDescription();
-      displayRoomName();
-    }
-  }
-
-  //Get user to go west if there is a room in that direction, if not it tells the user that they can't go that way
-  public void goWest() {
-    if (currentLocation.getWest() == null) {
-      System.out.println("You cannot go this way");
-    } else {
-      currentLocation = currentLocation.getWest();
+      currentLocation = direction;
     }
     if (currentLocation.getDiscovered()) { //Do not display room name when direction is null
       displayRoomName();
@@ -221,16 +176,20 @@ public class Adventure {
           displayLookDescription();
           break;
         case "go north", "north", "n":
-          goNorth();
+          //goNorth();
+          goDirection(currentLocation.getNorth());
           break;
         case "go south", "south", "s":
-          goSouth();
+          //goSouth();
+          goDirection(currentLocation.getSouth());
           break;
         case "go east", "east", "e":
-          goEast();
+          //goEast();
+          goDirection(currentLocation.getEast());
           break;
         case "go west", "west", "w":
-          goWest();
+          //goWest();
+          goDirection(currentLocation.getWest());
           break;
         case "go":
           System.out.println("Where do you want to go?");
