c265df4113c80da72500bf55901dc8300092176b
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-17 15:05:42 +0100

    Reformatted item array

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -2,19 +2,18 @@ package com.company;
 
 public class Item {
 
-  private String laserCutter;
-  private String fireExtinguisher;
-  private String repairTool;
-  private String survivalKnife;
-  private String flashLight;
-  private String powercell;
+  String[] items;
 
-  public Item() {
-    this.laserCutter = "lasercutter";
-    this.fireExtinguisher = "fire extinguisher";
-    this.repairTool = "repair tool";
-    this.survivalKnife = "survival knife";
-    this.flashLight = "flashlight";
-    this.powercell = "power cell";
+  public Item(String book, String the_great_gatsby) {
+    this.items[0] = "lasercutter";
+    this.items[1] = "fire extinguisher";
+    this.items[2] = "repair tool";
+    this.items[3] = "survival knife";
+    this.items[4] = "flashlight";
+    this.items[5] = "power cell";
+  }
+
+  public String[] getItems() {
+    return items;
   }
 }
